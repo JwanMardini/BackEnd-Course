527ef60868a71f8d215ba6d5c9e621b58c96a355
JwanMardini <jwan.99.mardini@gmail.com>
2024-02-23 10:30:28 +0100

    Exercise 4 is done

--- Exercises/Exercise1/Task1/index.js
+++ Exercises/Exercise1/Task1/index.js
@@ -1,19 +1,30 @@
 import express from "express";
 import compression from 'compression';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import fs from "fs";
+
 
 const app = express();
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 //register view engine
 app.set('view engine', 'ejs');
 
-// app.use(express.static('public'));
+app.use(express.static('public'));
+
 app.use(compression());
 
-
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     res.sendFile( "./public/index.html");
 });
+
+app.get("/show-form", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "form.html"));
+})
 
 app.get("/users", (req, res) => {
     const users = [
@@ -24,6 +35,12 @@ app.get("/users", (req, res) => {
     res.render("template", {users})
 
 });
+
+app.post("/submit-form", (req, res) => {
+    const {name, password} = req.body;
+    fs.appendFileSync("savedData.txt", `Name: ${name}, Password: ${password}\n`);
+    res.redirect("/");
+})
 
 app.listen(3000, () => {
     console.log("Server running at http://localhost:3000");
